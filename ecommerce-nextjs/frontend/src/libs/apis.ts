0f11051e08e69b61d0e50d276a7251138ce209a5
Yaw Amo Mensah <amomensah300@gmail.com>
2023-11-18 01:24:43 +0000

    adding edited api files for the frontend

--- ecommerce-nextjs/frontend/src/libs/apis.ts
+++ ecommerce-nextjs/frontend/src/libs/apis.ts
@@ -3,7 +3,7 @@ import sanityClient from "./sanity";
 import { Game } from "@/models/game";
 
 export const getCategories = async (): Promise<Category[]> => {
-    const query = `*[_type == 'category] {
+    const query = `*[_type == 'category'] {
         _id, 
         name,
         slug {current},
@@ -18,6 +18,7 @@ export const getCategories = async (): Promise<Category[]> => {
 export const getGames = async (): Promise<Game[]> => {
     const query = `*[_type == 'game'] {
         name,
+
         price,
         images,
         isFeatured,
@@ -36,3 +37,29 @@ export const getGames = async (): Promise<Game[]> => {
     const games: Game[] = await sanityClient.fetch({ query });
     return games;
 }
+
+export const getCategoryGames = async(slug: string): Promise<Game[]> => {
+    const query = `*[_type == 'game' && category->slug.current == '${slug}'] {
+        price,
+        images,
+        isFeatured,
+        isTrending,
+        slug,
+        quantity, 
+        description,
+        category->{
+            name,
+            subtitle
+        }
+    }`;
+
+    const games: Game[] = await sanityClient.fetch({ query });
+    return games;
+};
+
+export const getCategory = async (slug: string): Promise<Category> => {
+    const query = `*[_type == 'category' && slug.current == '${slug}'][0]`;
+
+    const category: Category = await sanityClient.fetch({ query });
+    return category;
+};
