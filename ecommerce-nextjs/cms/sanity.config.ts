bc1f58e3311135639a9d04686c344a6106d4d9df
Yaw Amo Mensah <amomensah300@gmail.com>
2023-11-18 01:20:53 +0000

    adding edited files for cms

--- ecommerce-nextjs/cms/sanity.config.ts
+++ ecommerce-nextjs/cms/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'WebStack_Portfolio_Project-Ecommerce_Nextjs',
 
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-  dataset: process.env.SANITY_STUDIO_DATASET!,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID as string,
+  dataset: process.env.SANITY_STUDIO_DATASET as string,
 
   plugins: [deskTool(), visionTool()],
 
