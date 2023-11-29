823f50a3beadfd33ed0aa3af6b255d9b7eb72925
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2023-11-29 02:49:20 +0000

    adding edited Header component file

--- ecommerce-nextjs/frontend/src/components/Header/Header.tsx
+++ ecommerce-nextjs/frontend/src/components/Header/Header.tsx
@@ -7,6 +7,8 @@ import { FcGoogle } from 'react-icons/fc';
 import { useAppDispatch } from "@/hooks/storeHook";
 import useCartTotals from "@/hooks/useCartTotals";
 import { toggleCart } from "@/redux/features/cartSlice";
+import Signup from "../Signup/Signup";
+import { useState } from "react";
 
 const Header = () => {
     const {
@@ -29,49 +31,56 @@ const Header = () => {
     const {totalQuantity} = useCartTotals();
     const dispatch = useAppDispatch();
 
+    const [isSignupFormOpen, setIsSignUpFormOpen] = useState(false);
 
-return (
-    <header className={header}>
-        <div className={container}>
-            <Link href='\' className={logoContainer}>
-                <h1 className={logo}>Logo</h1>
-            </Link>
+    const toggleForm = () => {
+        setIsSignUpFormOpen(!isSignupFormOpen);
+    };
 
-            <nav className={nav}>
-                <ul className={ul}>
-                    <li>
-                        <button className={link}
-                        onClick={() => dispatch(toggleCart())}>
-                            <span>Cart
-                            <AiOutlineShoppingCart className='inline-block text-3xl' />
-                            </span>
-                            <div className={cart}>{totalQuantity}</div>
-                        </button>
-                    </li>
+    return (<>
+        <Signup isSignupFormOpen={isSignupFormOpen} toogleForm={toggleCart}/>
+        <header className={header}>
+            <div className={container}>
+                <Link href='\' className={logoContainer}>
+                    <h1 className={logo}>Logo</h1>
+                </Link>
 
-                    <li className="flex items-center justify-center h-7">
-                        <Link href='/orders' className={orders}>
-                            Orders
-                        </Link>
-                        <button className={logoutBtn}>Logout</button>
-                        <button className={signupBtn}>Sign Up</button>
-                        <button className={signinBtn}>
-                            Sign In
-                            <FcGoogle
-                                style={{
-                                    fontSize: '25px',
-                                    cursor: 'pointer',
-                                    marginLeft: '12px',
-                                }}
-                                className={link} 
-                            />
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    );
+                <nav className={nav}>
+                    <ul className={ul}>
+                        <li>
+                            <button className={link}
+                            onClick={() => dispatch(toggleCart())}>
+                                <span>Cart
+                                <AiOutlineShoppingCart className='inline-block text-3xl' />
+                                </span>
+                                <div className={cart}>{totalQuantity}</div>
+                            </button>
+                        </li>
+
+                        <li className="flex items-center justify-center h-7">
+                            <Link href='/orders' className={orders}>
+                                Orders
+                            </Link>
+                            <button className={logoutBtn}>Logout</button>
+                            <button className={signupBtn} onClick={toggleForm}>Sign Up</button>
+                            <button className={signinBtn}>
+                                Sign In
+                                <FcGoogle
+                                    style={{
+                                        fontSize: '25px',
+                                        cursor: 'pointer',
+                                        marginLeft: '12px',
+                                    }}
+                                    className={link} 
+                                />
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </>
+        );
 }; 
 
 export default Header;
