b283124fb4f294e5c17832cdb698b367e83cb703
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2024-01-20 00:38:11 +0000

    adding orders table to the Orders page

--- ecommerce-nextjs/frontend/src/app/orders/page.tsx
+++ ecommerce-nextjs/frontend/src/app/orders/page.tsx
@@ -5,11 +5,59 @@ const Orders = async () => {
     const orderData: any = await fetchOrder('test@test.com');
 
     
-    return (
-        <div>
+    const Orders = async () => {
+        const orderData: any = await fetchOrder('test@test.com');
+    
+        return (
+            <div>
             <HeroSection />
-        </div>
-    );
+    
+            <div className="relative overflow-x-auto px-6 sm:px-12 md:px-20 lg:px-36 pd-40">
+                <table className="w-full sm text-left text-gray-400">
+                    <thead className="text-xs uppercase bg-gray-700 text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">Product(s)</th>
+                            <th scope="col" className="px-6 py-3">Unit Price</th>
+                            <th scope="col" className="px-6 py-3">Order Status</th>
+                            <th scope="col" className="px-6 py-3">Total Price</th>
+                        </tr>
+                    </thead> 
+                    
+                    <tbody>
+                        {orderData.map((order: any) => {
+                            const totalPrice = order.items.reduce((acc: any, item: any) => {
+                                const itemPrice = item.quantity * item.game.price;
+                                return acc + itemPrice;
+                            }, 0);
+    
+                            return ( <tr key={order._id} className="border-b bg-gray-800 border-gray-700">
+                                <th 
+                                scope="row"
+                                className="px-6 py-4 font-medium whitespace-nowrap text-white">
+                                    {order.items.map((item: any) => (<span key={item._id}>
+                                        {item.game.name} ({item.quantity}) <br />
+                                    </span>
+                                ))}
+                            </th>
+    
+                            <td className='px-6 py-4'>
+                                {order.items.map((item: any) => (<span key={item._id}>
+                                        {item.game.price} < br />
+                                    </span>
+                                ))}
+                            </td>
+                            <td className='px-6 py-4'>{order.orderStatus}</td>
+                            <td className='px-6 py-4'>{totalPrice}</td>
+                            </tr>
+                            
+                            )})}
+                    </tbody>
+                </table>
+    
+            </div>
+            </div>
+        )
+    }
 };
 
 export default Orders;
